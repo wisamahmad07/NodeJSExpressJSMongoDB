4bed9f40c66272d9ff245e69768b104626905829
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-08 10:17:07 +0500

    how require works use _.contains

---
+++ npm - Node Package Manager/index.js
@@ -0,0 +1,8 @@
+const _ = require("underscore");
+
+// core node module
+// file underscore.js or folder underscore/index.js
+// node_modules
+
+const result = _.contains([1, 2, 3], 2);
+console.log(result);
