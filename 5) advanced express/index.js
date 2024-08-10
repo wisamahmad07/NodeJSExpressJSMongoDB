2be26713e293df8d83a9c6587ff56a668568bc7e
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-10 12:05:50 +0500

    NODE_ENV CONFIGURATION console.log(`node_env is ${process.env.NODE_ENV}`); console.log(`appget is ${app.get("env")}`);

--- 5) advanced express/index.js
+++ 5) advanced express/index.js
@@ -8,9 +8,16 @@ const express = require("express");
 // const authentication = require("./middleware/authentication");
 const app = express();
 
+console.log(`node_env is ${process.env.NODE_ENV}`);
+console.log(`appget is ${app.get("env")}`);
+
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("tiny"));
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("in dev");
+}
+
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.static("public"));
 // app.use(logging);
