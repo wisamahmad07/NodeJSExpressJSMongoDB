9d9c83f3a8fa25c9a32cc6d7147f7c882f5cdb6e
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-11 14:29:54 +0500

    i just watch callback hell and solulu but dont need to learn it because for single we use callback but most of complex things or simple things we use async await or promises

--- 6) Async await/callback.js
+++ 6) Async await/callback.js
@@ -1,21 +1,21 @@
 console.log("before");
-getUser(1, (user) => {
+getUser((user) => {
   console.log("user", user);
 
-  getRepositories(user.name, (repo) => {
+  getRepositories((repo) => {
     console.log("user repos", repo);
   });
 });
 console.log("after");
 
-function getUser(id, callback) {
+function getUser(callback) {
   setTimeout(() => {
     console.log("connecting to db...");
-    callback({ id: id, name: "Mosh" });
+    callback({ id: 1, name: "Mosh" });
   }, 2000);
 }
 
-function getRepositories(username, callback) {
+function getRepositories(callback) {
   setTimeout(() => {
     console.log("getting repos");
     callback(["repo1", "repo2"]);
