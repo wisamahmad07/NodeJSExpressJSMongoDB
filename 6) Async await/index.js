8190ac3ac434efa627e948f424e0649b541a0504
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-11 13:50:42 +0500

    result is not immediately available because it is not ready

--- 6) Async await/index.js
+++ 6) Async await/index.js
@@ -1,5 +1,17 @@
+/* result is undefined because its not ready at that time
+   So, we have to use 
+   Callbacks
+   Promises
+   Async and await */
 console.log("before");
-setTimeout(() => {
-  console.log("coonecting to db...");
-}, 2000);
+const result = getUser();
+console.log(result);
+
 console.log("after");
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("connecting to db...");
+    return { id: id, name: "Mosh" };
+  }, 2000);
+}
