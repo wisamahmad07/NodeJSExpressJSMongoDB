d2af53dbfce5149b32fa19aed2bb33d79e279dab
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-12 15:39:50 +0500

    write a document instance to save in collection of Course in course_crud_api database

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/crud_api")
+  .connect("mongodb://localhost:27017/course_crud_api")
   .then(() => console.log("Successfully, Connected to MongDB..."))
-  .catch((err) => console.error("Error in connecting to MongDB", err));
+  .catch((err) => console.log("Error in connecting to MongDB", err));
 
 const courseSchema = new mongoose.Schema({
   name: String,
@@ -14,4 +14,11 @@ const courseSchema = new mongoose.Schema({
     default: Date.now,
   },
   isPublished: Boolean,
+});
+const Course = mongoose.model("Course", courseSchema);
+const course = new Course({
+  name: "Mongo course",
+  author: "wisam",
+  tags: ["mongodb", "mongo compass", "mongoose"],
+  isPublished: true,
 });
