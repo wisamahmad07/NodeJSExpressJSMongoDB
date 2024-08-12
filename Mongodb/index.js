9d4ef5ed750472bf187f53ad6bb8fe0aa140796a
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-12 15:33:45 +0500

    create courseSchema for each document(row) in Course collection(table)

--- Mongodb/index.js
+++ Mongodb/index.js
@@ -4,3 +4,14 @@ mongoose
   .connect("mongodb://localhost:27017/crud_api")
   .then(() => console.log("Successfully, Connected to MongDB..."))
   .catch((err) => console.error("Error in connecting to MongDB", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {
+    type: String,
+    default: Date.now,
+  },
+  isPublished: Boolean,
+});
