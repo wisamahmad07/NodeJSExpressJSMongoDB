f31c378911d6ded2fb04c5932f81505a18ca183e
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-13 13:59:51 +0500

    solution1.js exercise

---
+++ Mongodb/Exercise/solution1.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/mongo-exercises")
+  .then(() => console.log("connected"))
+  .catch((err) => console.log("error", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {
+    type: String,
+    default: Date.now,
+  },
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getCourse() {
+  const result = await Course.find({
+    isPublished: true,
+    tags: "backend",
+  })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+  console.log(result);
+}
+getCourse();
