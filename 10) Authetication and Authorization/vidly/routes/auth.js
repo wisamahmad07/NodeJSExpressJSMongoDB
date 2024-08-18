02dd68abf0ad8d68528ccb27c9b509cb340a0450
Wisam Ahmad <wisamahmad786@gmail.com>
2024-08-18 16:37:41 +0500

    Authenticating user

---
+++ 10) Authetication and Authorization/vidly/routes/auth.js
@@ -0,0 +1,34 @@
+const { User, validate } = require("../models/user");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const { valid } = require("joi");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error, value } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid email or password");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password");
+    res.send(true);
+  } catch (error) {
+    res.status(500).send("internal server error");
+  }
+});
+function validate(auth) {
+  const schema = Joi.object({
+    email: Joi.string().required().min(5).max(50).email(),
+    password: Joi.string().required().min(8).max(50),
+  });
+  return schema.validate(auth);
+}
+module.exports = router;
